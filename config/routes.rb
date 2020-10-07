0682a8dc03bb00816955bfcca7be95127af8ff46
tuanntg <tuanntg@nustechnology.com>
2020-10-07 21:16:31 +0700

    Migrate database and use Devise in Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'home_pages#index'
+  # devise_for :users
+
   resources :photos
   resources :albums
-  resources :users
+
+  devise_for :users, skip: :all
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
