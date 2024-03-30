2c6a185087e7b746f6fc6a533b1b5bec3b6695b8
Lucas Nguyen <giatuannguyen17071999@gmail.com>
2024-03-30 20:30:23 +0700

    Fix syntax error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
   skip_before_action :require_no_authentication, only: [:create]
-  skip_before_action :authenticate_user!
 end
